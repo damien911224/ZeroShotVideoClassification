fb72c808c9596b7fee114efdf1016f4fb3173b8b
damien911224 <damien911224@gmail.com>
2022-07-11 17:26:05 +0900

    Modified

--- auxiliary/auxiliary_activitynet.py
+++ auxiliary/auxiliary_activitynet.py
@@ -76,6 +76,7 @@ def save_clips2npy(sourcepath, sample):
     frames = []
     capture = cv2.VideoCapture(os.path.join(sourcepath, 'videos', fname))
     fps = capture.get(cv2.CAP_PROP_FPS)
+    print(fps)
     loc = [(round(l[0] * fps), round(l[1] * fps)) for l in loc]
     print(loc)
     count, loc_idx = 0, 0
