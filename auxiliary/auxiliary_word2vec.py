c2ac4c7e0b31c4b5dd9854700fb9e34f48096090
damien911224 <damien911224@gmail.com>
2022-07-12 19:31:10 +0900

    Modified

--- auxiliary/auxiliary_word2vec.py
+++ auxiliary/auxiliary_word2vec.py
@@ -35,8 +35,8 @@ def load_word2vec():
         wv_model = Word2Vec.load_word2vec_format('./assets/GoogleNews-vectors-negative300.bin', binary=True)
         wv_model.init_sims(replace=True)
         wv_model.save('./assets/GoogleNews')
-    print(len(wv_model.wv))
-    print(wv_model.wv.index_to_key(0))
+    print(len(wv_model))
+    # print(wv_model.vocab)
     exit()
     return wv_model
 
