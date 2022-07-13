77426fb53c7cb84d3e33d4b567243b76dbfa831f
damien911224 <damien911224@gmail.com>
2022-07-13 20:30:18 +0900

    Modified

--- auxiliary/auxiliary_dataset.py
+++ auxiliary/auxiliary_dataset.py
@@ -79,9 +79,11 @@ def get_kinetics(dataset='k700'):
         if len(x) < 2: continue
         # fnames.append(os.path.join(sourcepath, "Kinetics-{}".format(n_classes), x[4], x[0],
         #                            "{}_{:06d}_{:06d}.mp4".format(x[1], int(x[2]), int(x[3]))))
-        fnames.append(os.path.join(sourcepath, "Kinetics-{}".format(n_classes), "frames",
-                                   "{}_{:06d}_{:06d}".format(x[1], int(x[2]), int(x[3]))))
-        labels.append(x[0])
+        path = os.path.join(sourcepath, "Kinetics-{}".format(n_classes), "frames",
+                                   "{}_{:06d}_{:06d}".format(x[1], int(x[2]), int(x[3])))
+        if len(glob.glob(os.path.join(path, "images", "*.jpg"))) >= 200:
+            fnames.append(path)
+            labels.append(x[0])
 
     classes = sorted(np.unique(labels).tolist())
 
