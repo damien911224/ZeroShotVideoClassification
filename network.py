7c36c9305f0db5644b6055b87ec318abb2a209ab
damien911224 <damien911224@gmail.com>
2022-07-19 01:32:29 +0900

    Modified

--- network.py
+++ network.py
@@ -307,7 +307,7 @@ class Decoder(nn.Module):
             # pred_embeddings[end_flags] = torch.zeros_like(pred_embeddings[0])
             pred[end_flags] = torch.zeros_like(pred[0])
             all_preds.append(pred)
-            next_inp = torch.Tensor(self.wv_model[next_token]).view(bs, 1, 300).cuda()
+            next_inp = torch.Tensor(self.wv_model[next_token]).view(bs, 300).cuda()
             next_inp[end_flags] = torch.zeros_like(next_inp[:, 0])
             inp = torch.cat((inp, (self.word2input_proj(next_inp) + s_pos_embeds[:, i]).unsqueeze(1)), dim=1)
             # inp = torch.cat((inp, (self.word2input_proj(pred_embeddings) + s_pos_embeds[:, i]).unsqueeze(1)), dim=1)
