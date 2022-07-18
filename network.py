7963a8a2387937b59cd3e491cc13838bc842b64a
damien911224 <damien911224@gmail.com>
2022-07-18 23:28:08 +0900

    Modified

--- network.py
+++ network.py
@@ -336,7 +336,6 @@ class Encoder(nn.Module):
         self.wv_model = Word2Vec.load('./assets/GoogleNewsAdded', mmap='r')
         # self.embeddings = nn.Linear(len(self.wv_model), self.d_model, bias=False)
         self.s_pos_embeds = nn.Embedding(self.max_seq_len, self.d_model).weight
-            nn.Embedding(self.max_seq_len, self.d_model).weight.view(1, self.max_seq_len, self.d_model).cuda()
 
         self.special_tokens = nn.Embedding(2, self.d_model).weight.view(1, 2, self.d_model).cuda()
 
