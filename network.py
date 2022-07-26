e6459dc238023e234e7b7e8e36b4c619525e003f
damien911224 <damien911224@gmail.com>
2022-07-26 21:21:04 +0900

    Modified

--- network.py
+++ network.py
@@ -570,8 +570,9 @@ class Model(nn.Module):
                     text = ' '.join(text.split()).strip()
                     batch_word_samples.append(text)
                 word_samples.append(batch_word_samples)
-            word_feats = torch.stack(word_feats, dim=1)
-            _, w_s, w_l, w_c = word_feats.shape
+            word_feats = torch.cat(word_feats, dim=1)
+            w_s = self.num_sentences
+            _, w_l, w_c = word_feats.shape
             w_pos_embeds = (self.s_pos_embeds.weight.view(w_s, 1, self.d_model) +
                             self.l_pos_embeds.weight.view(1, w_l, self.d_model)).view(1, w_s * w_l, self.d_model)
             word_feats = (word_feats + w_pos_embeds.cuda()).detach()
