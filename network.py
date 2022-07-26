af826524347d7c7c2e1165b32576cc62de34f952
damien911224 <damien911224@gmail.com>
2022-07-26 22:18:26 +0900

    Modified

--- network.py
+++ network.py
@@ -492,7 +492,7 @@ class Model(nn.Module):
 
         self.d_model = 128
         self.num_sentences = 4
-        self.max_seq_len = self.decoding_len
+        self.max_seq_len = 16
         self.t_pos_embeds = nn.Embedding(2, self.d_model)
         self.h_pos_embeds = nn.Embedding(7, self.d_model)
         self.w_pos_embeds = nn.Embedding(7, self.d_model)
