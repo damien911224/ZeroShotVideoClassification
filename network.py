9f6a074fafd12ae60ad4d35d8fa377f17b4bfd2d
damien911224 <damien911224@gmail.com>
2022-07-28 15:23:33 +0900

    Modified

--- network.py
+++ network.py
@@ -576,7 +576,8 @@ class Model(nn.Module):
 
             # image_embeds = self.clip.compute_image_representation_from_image_instance(image_instances)
 
-            image_instances = self.clip_preprocess(image_instances).cuda()
+            image_instances = [self.clip_preprocess(image_instance) for image_instance in image_instances]
+            image_instances = torch.cat(image_instances).cuda()
             image_embeds = self.clip.encode_image(image_instances)
 
             # image_feats = image_embeds.view(bs, self.num_sentences, 512).detach() + \
